dd1ec44088e31f06eef7f222cc031234a77e1ad8
Rodrigo <rodrifernandezjr@gmail.com>
2019-12-14 16:41:51 -0300

    Se implementó la clase FileReader

--- app/src/main/java/softmobile/storemanagement/file/FileReader.java
+++ app/src/main/java/softmobile/storemanagement/file/FileReader.java
@@ -1,4 +1,36 @@
 package softmobile.storemanagement.file;
 
-public class FileReader {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader
+{
+    public List<String> readFile(InputStream inputStream)
+    {
+        try
+        {
+            return read(inputStream);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private List<String> read(InputStream inputStream) throws IOException
+    {
+        List<String> list = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+            list.add(line);
+        }
+        return list;
+    }
 }
